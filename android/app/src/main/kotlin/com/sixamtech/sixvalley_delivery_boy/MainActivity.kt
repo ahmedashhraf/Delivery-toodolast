362d47570069110412a838f695cd2ff3665bc54c
power tech <ahmedashrafelbahgi@gmail.com>
2024-11-22 02:31:09 +0200

    3.13.3

---
+++ android/app/src/main/kotlin/com/sixamtech/sixvalley_delivery_boy/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.todriverdo.delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+
+}
---
+++ lib/utill/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'dimensions.dart';
+final AlmaraiRegular = TextStyle(
+  fontFamily: 'Almarai',
+  fontSize: Dimensions.fontSizeDefault,
+  fontWeight: FontWeight.w400,
+);
+
+final AlmaraiMedium = TextStyle(
+  fontFamily: 'Almarai',
+  fontSize: Dimensions.fontSizeDefault,
+  fontWeight: FontWeight.w500,
+);
+
+final AlmaraiBold = TextStyle(
+  fontFamily: 'Almarai',
+  fontSize: Dimensions.fontSizeDefault,
+  fontWeight: FontWeight.w700,
+);
